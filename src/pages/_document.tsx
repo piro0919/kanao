1c615bcbfe488d143c724f183a2478db09ade7c1
piro <piro.haniwa@gmail.com>
2022-07-30 02:59:53 +0900

    fix: enable google fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,51 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link href="https://fonts.googleapis.com" rel="preconnect" />
+          <link
+            crossOrigin="true"
+            href="https://fonts.gstatic.com"
+            rel="preconnect"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap&text=有限会社金尾本店"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Shippori+Mincho&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
